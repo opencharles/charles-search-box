d21aa4eeabc1d3f1ac0b5b8e40ffa3f5ce6ca83e
kaymorey <moreirakatia@neuf.fr>
2017-01-14 18:50:48 +0100

    Default style results, separate css file

--- src/js/components/Input.js
+++ src/js/components/Input.js
@@ -122,7 +122,7 @@ export default class Input extends React.Component {
     render() {
       return (
         <div id="charles-search-wrapper">
-          <span id="charles-search-input-wrapper">
+          <div id="charles-search-input-wrapper">
           {
             this.state.keywords.length == 0 ?
               <input
@@ -136,7 +136,7 @@ export default class Input extends React.Component {
                 autoComplete="off"
               />
           }
-          </span>
+          </div>
         </div>
        );
     }
