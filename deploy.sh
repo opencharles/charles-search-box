444bbcaa577ab7b3653e41edf0b4479eb9cbacd8
amihaiemil <amihaiemil@gmail.com>
2017-02-08 23:32:17 +0200

    deployment script init

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+# Script to deploy the final build and some css files
+# to the repo amihaiemil/amihaiemil.github.io
+# That repo is a website hosted on Github pages and clients will fetch
+# the build and css from there.
+#
+# It uses the Github API to PUT the files.
+# We are not using git because rultor has a hard time cloning and using other
+# repos (we need to use SSH and the server doesn't trust the host, or HTTPS, where we
+# are prompted for user and password.)
+
+
+# script here...
+
