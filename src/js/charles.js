0145787c878ef2175831f083f4ac962099df01d6
amihaiemil <amihaiemil@gmail.com>
2017-06-16 10:57:51 +0300

    fix display timing

--- src/js/charles.js
+++ src/js/charles.js
@@ -4,12 +4,6 @@ import ReactDOM from "react-dom";
 import Search from "./components/Search";
 var $ = require('jquery');
 
-var searchDiv = document.getElementById("charles-search");
-if(searchDiv == null) {
-    searchDiv = document.createElement("div");
-    searchDiv.setAttribute("id", "charles-search");
-    document.body.appendChild(searchDiv);
-}
 
 var search = 'http://ec2-34-209-37-87.us-west-2.compute.amazonaws.com:8080/charles-rest/api/s/';
 var repo = $('script[id=charlesscript]').attr('repo');
@@ -25,6 +19,12 @@ if(repo) {
   $('head').append(
     '<link href="http://www.amihaiemil.com/css/charles/charles_light.css" type="text/css" rel="stylesheet"/>'
   )
+  var searchDiv = document.getElementById("charles-search");
+  if(searchDiv == null) {
+      searchDiv = document.createElement("div");
+      searchDiv.setAttribute("id", "charles-search");
+      document.body.appendChild(searchDiv);
+  }
   ReactDOM.render(
     <Search searchUrl={search + repo + "?index=0&size=" + size + "&kw="} placeholder={placeholder}/>,
     searchDiv
